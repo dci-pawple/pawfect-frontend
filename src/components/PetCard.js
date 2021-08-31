c7093ebb130cd34d6a00fc9f55faa1b0598ea844
magdasokolovic <magda.sokolovic@gmail.com>
2021-08-31 14:12:24 +0200

    created html structure and added 2 icons

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -2,10 +2,30 @@ import React from 'react'
 
 const PetCard = () => {
     return (
-        <div className="pet-card-container">
-            <h1>PET CARD</h1>
-            
-        </div>
+        // eslint-disable-next-line
+        <a href="#" className="card__container">
+            <div className="card__image">
+                {/* <img src="../images/charles-deluvio-pOUA8Xay514-unsplash.jpg" alt="a dog" /> */}
+            </div> 
+            <div className="card__content">
+                <div className="card__content--top">
+                    <div className="card__title">Lily</div>
+                    <img src="../icons/heart-regular.svg" alt="heart icon" />
+                </div>
+                <div className="card__description">
+                    <p>Female</p>
+                    <p>1 year old</p>
+                </div>
+                <div className="card__location">
+                    <div className="card__location--icon">
+                        <img src="../icons/location-arrow-solid.svg" alt="location-icon" />
+                    </div>
+                    <div className="card__location--name">
+                        <p>Berlin, Germany</p>
+                    </div>
+                </div>
+            </div>        
+        </a>
     )
 }
 
