43ac6a3afb10fcb88fc6754c2decb7da3968dd86
magdasokolovic <magda.sokolovic@gmail.com>
2021-08-31 14:31:14 +0200

    updated icons file

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,30 +1,33 @@
 import React from 'react'
+import { Heart, Location } from '../icons/icons'
 
 const PetCard = () => {
     return (
         // eslint-disable-next-line
-        <a href="#" className="card__container">
+        <a href="#" className="card">
             <div className="card__image">
                 {/* <img src="../images/charles-deluvio-pOUA8Xay514-unsplash.jpg" alt="a dog" /> */}
             </div> 
             <div className="card__content">
                 <div className="card__content--top">
                     <div className="card__title">Lily</div>
-                    <img src="../icons/heart-regular.svg" alt="heart icon" />
+                    <div className="card__like--icon">
+                        <Heart/>
+                    </div>
                 </div>
                 <div className="card__description">
                     <p>Female</p>
                     <p>1 year old</p>
                 </div>
-                <div className="card__location">
-                    <div className="card__location--icon">
-                        <img src="../icons/location-arrow-solid.svg" alt="location-icon" />
-                    </div>
-                    <div className="card__location--name">
-                        <p>Berlin, Germany</p>
-                    </div>
-                </div>
-            </div>        
+            </div> 
+            <div className="card__location">
+                 <div className="card__location--icon">
+                    <Location/>
+                 </div>
+                <div className="card__location--name">
+                      <p>Berlin, Germany</p>
+                 </div>
+            </div>       
         </a>
     )
 }
