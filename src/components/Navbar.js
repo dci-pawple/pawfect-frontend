16826969b271489dd5f1736ddba9f7329a83f8ea
juliasckr <jule.s@live.de>
2021-08-31 11:07:34 +0200

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="navigation">
-      <p>test</p>
+      <Link to="/">
+        <p>Hi</p>
+      </Link>
     </div>
   );
 }
