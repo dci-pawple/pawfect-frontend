67885be4d2432a33658e5de3aa00ad4e9ad26fa2
Dusan <dusan.chmelko@yahoo.com>
2021-08-31 14:43:19 +0200

    add categories to home page

---
+++ src/components/Categories.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// import dog from "../icons/dog1.png";
+
+const Categories = () => {
+  return (
+    <div className="container categories">
+      <div>
+        <i class="fas fa-dog fa-3x"></i>
+      </div>
+      <div>
+        <i class="fas fa-cat fa-3x"></i>
+      </div>
+      <div>
+        <i class="fas fa-crow fa-3x"></i>
+      </div>
+      <div>
+        <i class="fas fa-fish fa-3x"></i>
+      </div>
+      <div>
+        <i class="fas fa-dragon fa-3x"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
+
+import Categories from "../components/Categories";
 import PetCard from "../components/PetCard";
 
 const Home = () => {
   return (
     <>
       <div>HomePAGE</div>
-          <PetCard/>
-      </>
-    )
+      <PetCard />
+      <Categories />
+    </>
+  );
 };
 
 export default Home;
