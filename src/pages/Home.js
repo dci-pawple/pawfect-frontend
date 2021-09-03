e11890f72fb4463a9b74357213d9cef9bc515944
juliasckr <jule.s@live.de>
2021-09-03 10:58:12 +0200

    update navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Categories from "../components/Categories";
 import CarouselPets from "../components/CarouselPets";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Banner />
       <Categories />
       <div className="container">
         <h2>Newest pets</h2>
