e1c52f2bfcb61da5ad89ac91b4796cd1da89adf7
ppphiiil <info@philmeinwelt.com>
2021-09-03 18:08:33 +0200

    added all forms

--- src/App.js
+++ src/App.js
@@ -5,9 +5,31 @@ import "./style/App.scss";
 
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import {ThemeProvider, createTheme} from '@material-ui/core/styles';
+
+const theme =createTheme({
+palette: {
+    primary: {
+      light: '#ff9e9a',
+      main: '#f76c6c',
+      dark: '#bf3a41',
+      contrastText: '#fff',
+    },
+     secondary: {
+      light: '#464646',
+      main: '#1f1f1f',
+      dark: '#000000',
+      contrastText: '#fff',
+    },
+  },
+})
+
 
 const App = () => {
   return (
+    <ThemeProvider theme={theme}>
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
@@ -16,8 +38,15 @@ const App = () => {
         <Route path="/gallery">
           <Gallery />
         </Route>
+        <Route path="/registration">
+          <Registration />
+        </Route>
+         <Route path="/login">
+          <Login />
+        </Route>
       </Switch>
     </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
