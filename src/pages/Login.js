6286bfa905817cdf91ef1da912d7a8e6fc1c8bd2
juliasckr <jule.s@live.de>
2021-09-03 11:30:36 +0200

    start login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,42 @@
+import { Form, Formik } from "formik";
 import React from "react";
 import Navbar from "../components/Navbar";
 
 export default function Login() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <div className="form">
+        <Formik initialValues={{ email: "", password: "" }}>
+          {() => (
+            <Form>
+              <h2>Login</h2>
+              <input
+                type="email"
+                name="email"
+                //  onChange={handleChange}
+                //  onBlur={handleBlur}
+                // value={values.email}
+              />
+              {/* // {errors.email && touched.email && errors.email} */}
+              <input
+                type="password"
+                name="password"
+                // onChange={handleChange}
+                // onBlur={handleBlur}
+                // value={values.password}
+              />
+              {/* {errors.password && touched.password && errors.password} */}
+              <button
+                type="submit"
+                //  disabled={isSubmitting}
+              >
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
 }
