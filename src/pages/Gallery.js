07f3e27f711baae615ea5c3b61b1dea50e17967b
ppphiiil <info@philmeinwelt.com>
2021-09-10 14:35:23 +0200

    working on filter

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,55 +1,44 @@
-import React,{useContext,useEffect} from "react";
-import GalleryFilter from "../components/GalleryFilter";
-import PetCard from "../components/PetCard";
+import React, { useContext, useEffect } from 'react'
+import GalleryFilter from '../components/GalleryFilter'
+import PetCard from '../components/PetCard'
 import Dog3 from '../dummy/images/charles-deluvio-Mv9hjnEUHR4-unsplash.jpg'
-import MyContext from "../context/MyContext";
-
+import MyContext from '../context/MyContext'
 
 const Gallery = () => {
-  
-    const { filteredData, setFilteredData } = useContext(MyContext);
+  const { filteredData, setFilteredData } = useContext(MyContext)
 
-    useEffect(async () => {
-      try {
-                const response = await fetch( `http://localhost:4000/pets/`, {
-                    method: 'GET',
-                    mode: 'cors',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                   
-                } )
+   useEffect( async () => {
+   
+    try {
+      const response = await fetch(`http://localhost:4000/pets/`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
 
-                const data = await response.json()
-                setFilteredData(data.data)
-               
-                if ( !data.success ) {
-                   console.log("");
-                   
-                
-                } else {
-                    console.log("fetched all data");
+      const data = await response.json()
+      setFilteredData(data.data)
 
-                }
-            } catch ( err ) {
-                console.log( 'Error while filtering =>', err )
-            }
-    }, [])
+      console.log('fetched all data')
+    } catch (err) {
+      console.log('Error while filtering =>', err)
+    }
+    
+  }, [])
 
   return (
-    <div className="app-container container">
+    <div className='app-container container'>
       <h2>Pets available:</h2>
-      <GalleryFilter/>
-      <div className="gallery__grid-container">
-      { filteredData.map((pet, index) => (
-        <PetCard src={Dog3} petData={pet} key={index}/>
-      ))}
-      
+      <GalleryFilter />
+      <div className='gallery__grid-container'>
+        {filteredData.map((pet, index) => (
+          <PetCard src={Dog3} petData={pet} key={index} />
+        ))}
       </div>
-      
     </div>
   )
-  
-};
+}
 
-export default Gallery;
+export default Gallery
