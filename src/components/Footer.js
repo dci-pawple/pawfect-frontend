17b3539de95d235cba2a299aa863cc9a068d950b
Mo <m.alhaj.sn@gmail.com>
2021-09-10 11:10:44 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,77 +3,56 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <ul className="flex">
-        <li>
-          <h5 className="copyright">
-            <p>
-              All copyright &copy; for <b>Magda, Julia, Phil, Dusan and Mo</b>
-            </p>
-          </h5>
-        </li>
+      <aside className="copy">
+        <p>
+          All copyright &copy; for <b>Magda, Julia, Phil, Dusan and Mo</b>
+        </p>
+      </aside>
+      <aside className="social">
+        <a
+          href="https://www.facebook.com/Pawfect-100884952347295/"
+          target="blank"
+          className="footerstyle fa fa-lg fa-facebook"
+        ></a>
 
-        <li>
-          <a href="#" className="footerstyle fa fa-lg fa-facebook"></a>
-        </li>
-        <li>
-          <a href="#" className="footerstyle fa fa-lg fa-instagram"></a>
-        </li>
-        <li>
-          <a href="#" className="footerstyle fa fa-lg fa-twitter"></a>
-        </li>
-        <li>
-          <a href="#" className="footerstyle fa fa-lg fa-linkedin-square"></a>
-        </li>
+        <a
+          href="https://www.instagram.com/pawfect.7/"
+          target="blank"
+          className="footerstyle fa fa-lg fa-instagram"
+        ></a>
 
-        <li id="help">
-          <h4 className="footer-list-header"></h4>
-        </li>
-        <li>
-          <a
-            href=""
-            className="footerstyle generic-anchor footer-list-anchor"
-            itemprop="significantLink"
-          >
-            CONTACT
-          </a>
-        </li>
-        <li>
-          <a
-            href=""
-            className="footerstyle generic-anchor footer-list-anchor"
-            itemprop="significantLink"
-          >
-            About us
-          </a>
-        </li>
-        <li id="faq">
-          <a
-            href=""
-            className="footerstyle generic-anchor footer-list-anchor"
-            itemprop="significantLink"
-          >
-            FAQ
-          </a>
-        </li>
-        <li id="terms">
-          <a
-            href=""
-            className="footerstyle generic-anchor footer-list-anchor"
-            itemprop="significantLink"
-          >
-            Terms
-          </a>
-        </li>
-        <li id="privacy">
-          <a
-            href=""
-            itemprop="significantLink"
-            className="footerstyle generic-anchor footer-list-anchor"
-          >
-            Privacy
-          </a>
-        </li>
-      </ul>
+        <a
+          href="https://twitter.com/pawfect7/"
+          target="blank"
+          className="footerstyle fa fa-lg fa-twitter"
+        ></a>
+
+        <a
+          href="https://www.linkedin.com/in/paw-fect-07a739220/"
+          target="blank"
+          className="footerstyle fa fa-lg fa-linkedin-square"
+        ></a>
+      </aside>
+
+      <aside className="help">
+        <ul>
+          <li>
+            <a href="">Contact</a>
+          </li>
+          <li>
+            <a href="">About us</a>
+          </li>
+          <li>
+            <a href="">FAQ</a>
+          </li>
+          <li>
+            <a href="">Terms</a>
+          </li>
+          <li>
+            <a href="">Privacy</a>
+          </li>
+        </ul>
+      </aside>
     </footer>
   );
 }
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,6 +2,8 @@ import React from "react";
 
 const Gallery = () => {
   return <div>Gallery</div>;
+  
 };
 
 export default Gallery;
+
