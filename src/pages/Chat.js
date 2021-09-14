5b63b54569fb8a7f1aa7ccefad9774efbe38d6e8
Dusan <dusan.chmelko@yahoo.com>
2021-09-14 16:13:57 +0200

    trying chatengine

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
 
+function ChatList() {
+  return <div>chatlist</div>;
+}
+
 export default function Chat() {
   return (
     <div className="app-container container">
@@ -8,6 +12,7 @@ export default function Chat() {
         projectID="b095d165-217e-4a79-b190-6fb4706e857a"
         userName="Julia"
         userSecret="pawfect1234"
+        renderChatList={(chatAppState) => <ChatList {...chatAppState} />}
       />
     </div>
   );
