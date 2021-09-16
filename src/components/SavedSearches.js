0d914bbfe9cf817646bac1227225179b79582f56
magdasokolovic <magda.sokolovic@gmail.com>
2021-09-16 14:46:39 +0200

    working on SavedSearches page

--- src/components/SavedSearches.js
+++ src/components/SavedSearches.js
@@ -3,35 +3,50 @@ import PetCard from "./PetCard";
 import MyContext from "../context/MyContext";
 
 const SavedSearches = () => {
-	const [favouritesList, setFavouritesList] = useState([]);
+	// const [favouritesList, setFavouritesList] = useState([]);
 	// new use state of a pet object
 	
 	const { userId } = useContext(MyContext);
+	const { filteredData, setFilteredData } = useContext(MyContext)
+
+	console.log("filteredData => ", setFilteredData)
 
 	useEffect(() => {
-		console.log("userId => ", userId);
 		const fetchFavourites = () => {
-			fetch(`http://localhost:4000/users/${userId}`)
-				.then(data => data.json())
-				.then(res => {
-					console.log("pet saved to favourites", res.data.savedFavorites);
-					setFavouritesList(res.data.savedFavorites);
-				})
-				.catch(err => console.log(err.response));
-		};
-		fetchFavourites();
-	}, [userId]);
+			fetch(`http://localhost:4000/pets/filter?favorites=true&userId=${userId}`)
+			.then(data => data.json())
+			.then(response => console.log("response in favourites =>", response))
+			.catch(err => console.error("error in favourites =>", err))
+		}
+
+		fetchFavourites()
+	}, [])
+	
+	// useEffect(() => {
+	// 	console.log("userId => ", userId);
+	// 	const fetchFavourites = () => {
+	// 		fetch(`http://localhost:4000/users/${userId}`)
+	// 			.then(data => data.json())
+	// 			.then(res => {
+	// 				console.log("pet saved to favourites", res.data.savedFavorites);
+	// 				setFavouritesList(res.data.savedFavorites);
+	// 			})
+	// 			.catch(err => console.log(err.response));
+	// 	};
+	// 	fetchFavourites();
+	// }, [userId]);
+
 
 	return (
 		<div className='app-container container'>
 			<h2>Saved Searches</h2>
 			<div className='gallery__grid-container'>
-				{favouritesList.length &&
+				{/* {favouritesList.length &&
 					favouritesList.map((petId, index) => {
 						console.log("vgdgdfhg");
 						return <PetCard pet={petId} key={index} />;
 					})}
-					{console.log("favourite List =>", favouritesList)}
+					{console.log("favourite List =>", favouritesList)} */}
 			
 			</div>
 		</div>
