7c47e4a3c7e759667d4014ff05ad12ed50af2751
magdasokolovic <magda.sokolovic@gmail.com>
2021-09-16 11:12:52 +0200

    updated userId in Container

--- src/context/Container.js
+++ src/context/Container.js
@@ -6,7 +6,7 @@ const Container = ({ children }) => {
     localStorage.getItem("user") ? true : false
   );
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
-  const [userId, setUserId] = useState(login ? user._id : false);
+  const [userId, setUserId] = useState(login ? user._id : null);
   const [pet, setPet] = useState({});
   const [petId, setPetId] = useState(null);
   const [users, setUsers] = useState([]);
