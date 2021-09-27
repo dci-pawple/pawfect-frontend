21f319227a28e94ca34592e65d13c0972c17d425
magdasokolovic <magda.sokolovic@gmail.com>
2021-09-27 12:14:56 +0200

    .

--- src/components/CarouselPets.js
+++ src/components/CarouselPets.js
@@ -19,7 +19,7 @@ const CarouselPets = () => {
         // process.env.REACT_APP_BACKEND_URL
         // http://localhost:4000/
         await fetch(
-          `${process.env.REACT_APP_BACKEND_URL}pets`
+          process.env.REACT_APP_BACKEND_URL + "pets/"
         )
           .then((data) => data.json())
           .then((res) => {
